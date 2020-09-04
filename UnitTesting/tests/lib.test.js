62ee9e6e71950cb6055415cb509ac4e48952b9d6
SaranshDawra <saransh2000dawra@gmail.com>
2020-09-04 20:42:27 +0530

    Test updated

--- UnitTesting/tests/lib.test.js
+++ UnitTesting/tests/lib.test.js
@@ -1,4 +1,6 @@
 const lib = require("../lib");
+const db = require('../db');
+const mail = require('../mail');
 
 describe("absolute", () => {
     it("should return a positive number if input is positive", () => {
@@ -82,3 +84,37 @@ describe("registerUser", () => {
     });
 });
 
+describe('applyDiscount', () => {
+    it('Should apply 10% discount if customer ha more that 10 points', () => {
+        db.getCustomerSync = function(customerId) {
+            console.log('Fake Reading Customer...');
+            return {id: customerId, points: 20}
+        }
+        const order = {customerId: 1, totalPrice: 10}
+        lib.applyDiscount(order);
+        expect(order.totalPrice).toBe(9);
+    });
+});
+
+describe('notifyCustomer', () => {
+    it('Should send an email to the customer', () => {
+
+        db.getCustomerSync = jest.fn().mockReturnValue( {email: 'a'} ); 
+
+        // db.getCustomerSync = function(customerId) {
+        //     return {email: 'a'}
+        // }
+
+        mail.send = jest.fn();
+
+        // let mailSent = false;
+        // mail.send = function(email, message) {
+        //     mailSent = true;
+        // }
+
+        lib.notifyCustomer( {customerId: 1} );
+        expect(mail.send).toHaveBeenCalled();
+        expect(mail.send.mock.calls[0][0]).toBe('a');
+        expect(mail.send.mock.calls[0][1]).toMatch(/order/);
+    });
+});
